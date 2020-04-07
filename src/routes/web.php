f27c1bfead02b013670e16bab8dbf7fdaf0a1465
@higakikeita <higaki.keichan@gmail.com>
2020-04-07 20:04:03 +0900

    ルーティングの設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,3 +15,4 @@ Route::get('/', 'PostsController@index')->name('top');
 Route::resource('posts', 'PostsController', ['only' => ['create', 'store']]);
 Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show']]);
 Route::resource('comments', 'CommentsController', ['only' => ['store']]);
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show', 'edit', 'update']]);
