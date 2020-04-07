fa916ae0a4f7327d4aed3a224bf88a8e9e6d6e3e
@higakikeita <higaki.keichan@gmail.com>
2020-04-07 18:23:29 +0900

    コメント追加機能実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', 'PostsController@index')->name('top');
 Route::resource('posts', 'PostsController', ['only' => ['create', 'store']]);
 Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show']]);
+Route::resource('comments', 'CommentsController', ['only' => ['store']]);
